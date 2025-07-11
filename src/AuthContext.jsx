02222c98aa9a3630d474c90e17a796124d01e90f
milandiaz <milandiaz2000@hotmail.com>
2025-07-11 11:37:46 -0500

    First commit

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -6,11 +6,31 @@ const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [token, setToken] = useState();
-  const [location, setLocation] = useState("GATE");
+  const [location, setLocation] = useState("GATE"); 
+  const [error, setError] = useState(null);
 
   // TODO: signup
+  const signup = async () => {
+    try {
+      const response = await fetch(`${API}/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({  }),
+      });
+      const result = await response.json();
+      setToken(result.token);
+      setLocation("TABLET");
+    } catch (err) {
+    setError(err.message)
+  }
+  }
 
   // TODO: authenticate
+   const authenticate = async (token) => {
+    try {
+      const response = await fetch(`${API}/authenticate`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
 
   const value = { location };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
